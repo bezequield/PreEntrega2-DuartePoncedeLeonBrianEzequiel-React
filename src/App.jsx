bf0c4872dba1173b4a22e19f56b94b8241ead7d8
Ezequiel DPdL <brianezequielduarte@gmail.com>
2023-12-13 23:21:02 -0300

    editando actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import NavBar from './components/NavBar/NavBar'
 import CardContainer from './components/CardContainer/CardContainer'
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer'
-
-
 
 
 function App() {
@@ -19,8 +16,6 @@ function App() {
           <Route path='*' element={ <Navigate to='/' /> } /> 
           <Route path='/detail/:pid' element={ <ItemDetailContainer /> } />   
 
-
-          {/* <Route path='/est/:pid' element={ <CardContainerBodyItem /> } / >  */}
         </Routes>
 
         
